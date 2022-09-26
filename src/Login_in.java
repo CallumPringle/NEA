d9abfa56891f7c3c1c38a3348ffd994714cd7773
CallumPringle <pringcal@hotmail.com>
2022-09-26 14:54:07 +0100

    login framework done

--- src/Login_in.java
+++ src/Login_in.java
@@ -16,14 +16,30 @@ import java.util.concurrent.CountDownLatch;
 public class Login_in {
     public static void login() {
         JFrame frame = new JFrame("Login");
-        frame.setSize(600,300);
+        frame.setSize(400,150);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //username
         JTextField tf = new JTextField(20);
-        tf.setBounds(30,30,100,30);
+        JLabel username = new JLabel("Username:");
+        tf.setBounds(70,25,300,20);
+        username.setBounds(0,tf.getHeight(),100,30);
         frame.add(tf);
+        frame.add(username);
+
+        //password
+        JTextField pWordtf = new JTextField(20);
+        JLabel password = new JLabel("Password:");
+        pWordtf.setBounds(70,50,300,20);
+        password.setBounds(0,pWordtf.getY()-5,100,30);
+        System.out.println(pWordtf.getHeight() + " user" + tf.getHeight());
+        frame.add(password);
+        frame.add(pWordtf);
+
+
         frame.setLayout(null);
         Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
-        frame.setLocation((size.width/2-300),(size.height/2-150));
+        frame.setLocation((size.width/2-(frame.getWidth()/2)),(size.height/2-(frame.getHeight()/2)));
         frame.setVisible(true);
     }
 
